1aa0e5ed7a772cdfb232d5bc57fd4a0c7dbf8517
chinazaaa-6th <146667851+chinazaaa-6th@users.noreply.github.com>
2023-10-12 23:53:56 +0100

    Added date, styling and js

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let year = now.getFullYear();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let fullDate = `${date}, ${day} ${month}, ${year}`;
+dateElement = document.querySelector("#date");
+dateElement.innerHTML = fullDate;
+
+function formatDate(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let year = now.getFullYear();
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let datee = now.getDate();
+
+  let fullDate = `${day}, ${month} ${datee}, ${year}`;
+  return `${day}, ${month} ${datee}, ${year}`;
+}
+function showForecast(response) {
+  console.log(response.data.daily);
+  let futureElement = document.querySelector("#future");
+  let futureHTML = `<div class="row">`;
+  let dayss = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
+  dayss.forEach(function (dayy) {
+    futureHTML =
+      futureHTML +
+      `
+        <div class="col">
+          <div class="forcast-date">${dayy}</div>
+          <div class="forecast-icon">☀</div>
+          <span class="forcast-temp">28°C</span>
+        </div>`;
+  });
+  futureElement.innerHTML = futureHTML;
+  futureHTML = `</div>`;
+}
+function getForecast(coordinates) {
+  let key = "32fob4398470td4a73fb1e1ffb79ad6a";
+  let url = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${key}&units=metric`;
+  axios.get(url).then(showForecast);
+}
 function showTemp(response) {
   document.querySelector("#location").innerHTML = response.data.city;
   document.querySelector("#digit").innerHTML = Math.round(
@@ -17,6 +106,7 @@ function showTemp(response) {
       `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
   celTemp = response.data.temperature.current;
+  getForecast(response.data.coordinates);
 }
 
 function getCity(cityy) {
@@ -41,6 +131,7 @@ function getCurrent(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(findLocation);
 }
+
 function getFarTemp(event) {
   event.preventDefault();
   let faTemp = (celTemp * 9) / 5 + 32;
@@ -52,6 +143,17 @@ function getCelTemp(event) {
   event.preventDefault();
   let number = document.querySelector("#digit");
   number.innerHTML = Math.round(celTemp);
+}
+
+function getCondition(melbourne) {
+  let key = "32fob4398470td4a73fb1e1ffb79ad6a";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${Melbourne}&key=${key}&units=metric`;
+  axios.get(url).then(showTemp);
+}
+function showFrequent(event) {
+  event.preventDefault();
+  melbor = document.querySelector("#location");
+  getCondition(melbourne);
 }
 
 let celTemp = null;
@@ -69,3 +171,6 @@ farLink.addEventListener("click", getFarTemp);
 
 let celLink = document.querySelector("#cel");
 celLink.addEventListener("click", getCelTemp);
+
+let melCity = document.querySelector("#mel");
+melCity.addEventListener("click", showFrequent);
